c2a1d3425d13f117674d5b9623d284faa61db480
ishwa03 <email-12a.ishwakochhar@gmail.com>
2025-04-28 21:22:30 +0530

    Added button to navigate to predict data page

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
@@ -39,4 +40,54 @@ def predict_datapoint():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")        
-   
+"""
+
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+import webbrowser
+import threading
+import time
+
+application = Flask(__name__)
+app = application
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline = PredictPipeline()
+        print("Mid Prediction")
+        results = predict_pipeline.predict(pred_df)
+        print("After Prediction")
+
+        return render_template('home.html', results=results[0])
+
+# Function to open browser in a separate thread
+def open_browser():
+    time.sleep(1)  # slight delay to ensure the server starts first
+    webbrowser.open_new("http://127.0.0.1:5000/")
+
+if __name__ == "__main__":
+    threading.Thread(target=open_browser).start()
+    app.run(host="0.0.0.0", port=5000)
